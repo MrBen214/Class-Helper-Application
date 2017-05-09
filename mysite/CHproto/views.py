bdafc4743d126565a5c36c9d50388016552c1f66
MrBen214 <Mrben214@gmail.com>
2017-05-09 15:31:35 -0400

    Update views.py

--- mysite/CHproto/views.py
+++ mysite/CHproto/views.py
@@ -187,19 +187,91 @@ def attendance(request, course_id):
     m3_total = {}
     m4_total = {}
 
+    w1_a = {}
+    w2_a = {}
+    w3_a = {}
+    w4_a = {}
+    w5_a = {}
+    w6_a = {}
+    w7_a = {}
+    w8_a = {}
+    w9_a = {}
+    w10_a = {}
+    w11_a = {}
+    w12_a = {}
+    w13_a = {}
+    w14_a = {}
+    w15_a = {}
+    w16_a = {}
+
+    w1_total = {}
+    w2_total = {}
+    w3_total = {}
+    w4_total = {}
+    w5_total = {}
+    w6_total = {}
+    w7_total = {}
+    w8_total = {}
+    w9_total = {}
+    w10_total = {}
+    w11_total = {}
+    w12_total = {}
+    w13_total = {}
+    w14_total = {}
+    w15_total = {}
+    w16_total = {}
+
     all_totals = {}
+    week_totals = {}
 
     students = []
 
     for roster_entry in roster_entries:
         curr_student = Student.objects.get(pk=roster_entry.student.pk)
         students.append(curr_student)
-
+        print (str(roster_entry.pk))
+        for attend in Attendance.objects.all():
+            print (attend.roster)
         #attendances[curr_student] = Attendance.objects.filter(roster=roster_entry.pk).order_by('date')
         m1_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=1).order_by('date')
         m2_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=2).order_by('date')
         m3_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=3).order_by('date')
         m4_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, month_number=4).order_by('date')
+
+        w1_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=1).order_by('date')
+        w2_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=2).order_by('date')
+        w3_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=3).order_by('date')
+        w4_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=4).order_by('date')
+        w5_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=5).order_by('date')
+        w6_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=6).order_by('date')
+        w7_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=7).order_by('date')
+        w8_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=8).order_by('date')
+        w9_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=9).order_by('date')
+        w10_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=10).order_by('date')
+        w11_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=11).order_by('date')
+        w12_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=12).order_by('date')
+        w13_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=13).order_by('date')
+        w14_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=14).order_by('date')
+        w15_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=15).order_by('date')
+        w16_a[curr_student] = Attendance.objects.filter(roster=roster_entry.pk, week_number=16).order_by('date')
+
+        #m1_total[curr_student] = 0
+        w1_total = 0
+        w2_total = 0
+        w3_total = 0
+        w4_total = 0
+        w5_total = 0
+        w6_total = 0
+        w7_total = 0
+        w8_total = 0
+        w9_total = 0
+        w10_total = 0
+        w11_total = 0
+        w12_total = 0
+        w13_total = 0
+        w14_total = 0
+        w15_total = 0
+        w16_total = 0
 
         #m1_total[curr_student] = 0
         m1_total = 0
@@ -207,8 +279,11 @@ def attendance(request, course_id):
         m3_total = 0
         m4_total = 0
 
+        print ("attendances found " + str(len(Attendance.objects.filter(month_number=1))))
+
         for value in m1_a[curr_student]:
             m1_total += value.hours_attended
+            print ("attendances found " )
 
         for value in m2_a[curr_student]:
             m2_total += value.hours_attended
@@ -219,17 +294,67 @@ def attendance(request, course_id):
         for value in m4_a[curr_student]:
             m4_total += value.hours_attended
 
+        for value in w1_a[curr_student]:
+            w1_total += value.hours_attended
+
+        for value in w2_a[curr_student]:
+            w2_total += value.hours_attended
+
+        for value in w3_a[curr_student]:
+            w3_total += value.hours_attended
+
+        for value in w4_a[curr_student]:
+            w4_total += value.hours_attended
+
+        for value in w5_a[curr_student]:
+            w5_total += value.hours_attended
+
+        for value in w6_a[curr_student]:
+            w6_total += value.hours_attended
+
+        for value in w7_a[curr_student]:
+            w7_total += value.hours_attended
+
+        for value in w8_a[curr_student]:
+            w8_total += value.hours_attended
+        
+        for value in w9_a[curr_student]:
+            w9_total += value.hours_attended
+
+        for value in w10_a[curr_student]:
+            w10_total += value.hours_attended
+
+        for value in w11_a[curr_student]:
+            w11_total += value.hours_attended
+
+        for value in w12_a[curr_student]:
+            w12_total += value.hours_attended
+
+        for value in w13_a[curr_student]:
+            w13_total += value.hours_attended
+
+        for value in w14_a[curr_student]:
+            w14_total += value.hours_attended
+
+        for value in w15_a[curr_student]:
+            w15_total += value.hours_attended
+
+        for value in w16_a[curr_student]:
+            w16_total += value.hours_attended
+
         overall_hours = m1_total+m2_total+m3_total+m4_total
 
         all_totals[curr_student] = [m1_total, m2_total, m3_total, m4_total, overall_hours]
+        week_totals[curr_student] = [w1_total, w2_total, w3_total, w4_total, w5_total, w6_total, w7_total, w8_total, w9_total, w10_total, w11_total, w12_total, w13_total, w14_total, w15_total, w16_total]
 
     context = {
         'course': course,
-        'm1_total': m1_total,
-        'm2_total': m2_total,
-        'm3_total': m3_total,
-        'm4_total': m4_total,
+        #'m1_total': m1_total,
+        #'m2_total': m2_total,
+        #'m3_total': m3_total,
+        #'m4_total': m4_total,
         'all_totals': all_totals,
+        'week_totals': week_totals,
         #'attendances': attendances.items(),
         'roster_entries': roster_entries,
         'students': students,
